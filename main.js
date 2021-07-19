2db8a773f51ebb43b9bc36c31b9be65a3ae2f952
fuyoo <fuyoo@fuyoo.cc>
2021-07-19 16:25:27 +0800

    complete 1.0.4

---
+++ main.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const dgram = require('dgram')
+const os = require('os')
+    /**
+     * define fetch ip methods
+     */
+const fetch_ip = (dns) => {
+    return new Promise((resolve, reject) => {
+        if (process.version.split(".")[0].replace('v') < 12) {
+            reject('node.js version must be >= 12.0.0')
+            return
+        }
+        const socket = dgram.createSocket('udp4')
+        socket.connect(80, dns || '4.2.2.1', e => {
+            resolve(socket.address().address)
+            socket.close()
+        })
+        socket.on('error', e => resolve('127.0.0.1'))
+    })
+}
+
+/**
+ * is cli show ip?
+ */
+const [, , show] = process.argv
+
+/**
+ * show？
+ */
+if (show) {
+    fetch_ip().then(ip => {
+            if (process.platform === 'win32') {
+                console.log('\u001b[32m' + ip + '\u001b[0m')
+                return
+            }
+            console.log('\033[32m' + ip + '\033[39m')
+        })
+        .catch(e => {
+            console.log(e)
+        })
+    return
+}
+
+module.exports = fetch_ip
---
+++ readme.md
@@ -0,0 +1,35 @@
+# real_ip package and cli tool
+
+## support nodejs version
+
+version >= 12.0.0
+
+## how to use?
+
+### as a cli tool
+
+1. `npm i -g @fuyoo/real_ip`
+
+2. `execute command ip show`
+
+### as a node.js package
+
+1. `npm i @fuyoo/real_ip`
+
+2. `require this package`
+    ```javascript
+    const real_ip = require('@fuyoo/real_ip');
+    (async () => {
+       const res = await real_ip();
+       console.log(res);
+    })
+    ```
+
+## get ip principle?
+
+this package use udpV4 to connect dns ip addresses.
+
+when connect successed we close this connection.
+
+get the used local ip addres.
+
